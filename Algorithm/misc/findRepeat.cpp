b7d57715f3c499e140d760c1fc0d7485ba35174b
huxiandong <huxiandong@gmail.com>
2017-02-28 10:31:16 +0800

    misc: other methods to find repeat number in array
    Signed-off-by: huxiandong <huxiandong@gmail.com>

--- Algorithm/misc/findRepeat.cpp
+++ Algorithm/misc/findRepeat.cpp
@@ -6,7 +6,10 @@
  * 要求O(1)空间和O(n)时间。
  */
 
-static int findRepeatNumber(int a[], int n)
+/**
+ * 基于基数排序的思想
+ */
+static int findRepeatNumber1(int a[], int n)
 {
 	/**
 	 * 虽然有二重循环，但每个元素只会被访问一次
@@ -23,6 +26,49 @@ static int findRepeatNumber(int a[], int n)
 	return -1;
 }
 
+/**
+ * 符号位标记法
+ * 这种通过取负来判断元素是否重复访问的方法对于数组第0个元素为0并且数据中只有0重复时无法找出正解
+ */
+static int findRepeatNumber2(int a[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		if (a[i] > 0) {
+			if (a[a[i]] < 0) { // 已经被标上负值了，有重复
+				return a[i];
+			} else {
+				a[a[i]] = -a[a[i]]; // 记为负
+			}
+		} else {
+			if (a[-a[i]] < 0) { // 已经被标上负值了，有重复
+				return -a[i];
+			} else {
+				a[-a[i]] = -a[-a[i]]; // 记为负
+			}
+		}
+	}
+
+	return -1;
+}
+
+/**
+ * 因为对0取负导致上面的算法有缺陷，沿着上面的标记法的思路，
+ * 采用加n的方法，通过判断元素是否大于或等于n就能决定该元素是否被访问过了
+ */
+static int findRepeatNumber3(int a[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		int realIndex = a[i] >= n ? a[i] - n : a[i];
+		if (a[realIndex] >= n) {
+			return realIndex;
+		} else {
+			a[realIndex] += n;
+		}
+	}
+
+	return -1;
+}
+
 void findRepeatTest()
 {
 	const int MAX = 10;
@@ -30,7 +76,9 @@ void findRepeatTest()
 	printf("输入数组为：	");
 	printArray(a, MAX);
 
-	int number = findRepeatNumber(a, MAX);
+	//int number = findRepeatNumber1(a, MAX);
+	//int number = findRepeatNumber2(a, MAX);
+	int number = findRepeatNumber3(a, MAX);
 	if (number != -1) {
 		printf("找到重复元素：%d\n", number);
 	} else {
